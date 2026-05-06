125ce6f18595bf1d2c9cee232725cac659508ec1
Ty-Prince <llalu5548@gmail.com>
2026-05-06 21:23:22 +0530

    Revert "improve functinality"
    This reverts commit b70984f40431b92db19d7c30a360bcc4b872bb09.

--- lib/actions/book.actions.ts
+++ lib/actions/book.actions.ts
@@ -6,13 +6,21 @@ import { escapeRegex, genrateslug, serializedata } from "@/lib/utils";
 import Book from "@/database/models/book.model";
 import BookSegment from "@/database/models/bookSegment.model";
 import mongoose from "mongoose";
+import { auth } from "@clerk/nextjs/server";
 
 
 export const getAllbooks = async()=>{
     try {
         await connectToDatabase();
 
-        const books = await Book.find().sort({ createdAt: -1 }).lean();
+        const {userId} = await auth();
+
+        if(!userId){
+            return serializedata({ success: false, data : [],error: "Unauthorized" });
+        }
+        const query = { clerkId: userId };
+
+        const books = await Book.find(query).sort({ createdAt: -1 }).lean();
         return serializedata({ success: true, data: books.map(serializedata) });
         
     }catch(e){
@@ -115,8 +123,15 @@ export const saveBookSegments = async (bookId: string, clerkId: string, segments
 export const getbookbyslug = async (slug: string) => {
     try {
         await connectToDatabase();
+        const {userId} = await auth();
 
-        const book = await Book.findOne({ slug }).lean();
+        if(!userId){
+            return serializedata({ success: false, data : [],error: "Unauthorized" });
+        }
+        const query = { clerkId: userId };
+
+        const books = await Book.find(query).sort({ createdAt: -1 }).lean();
+        const book = await books.find((b) => b.slug === slug);
 
         if (!book) {
             return serializedata({ success: false, error: "Book not found" ,data : '' });
